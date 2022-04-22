7a349cd01e4a8cd933311fd56cd8ed62f19b9dd9
Luana <Luana@DESKTOP-ODA6GJK>
2022-04-21 22:32:40 -0300

    Adicionar arquivos de projeto.

---
+++ ARRAYS_Vetores_CSharp-12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ARRAYS_Vetores_CSharp_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { "Maria", "João", "Gabriel", "Fernanda" };
+
+            string[] nomes2 = new string[] { "Carla", "Rodrigo", "Joaquina", "Pedro" };
+
+
+            Console.WriteLine(nomes[2]);
+
+            Console.WriteLine(nomes2[0]);
+        }
+    }
+}
